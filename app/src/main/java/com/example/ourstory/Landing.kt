491e6056f8692405ebc4f526407ed0c8f504a243
Ryuga Eka <ryugasetiawan19@gmail.com>
2021-04-10 23:36:09 +0700

    fixed
    edan lah anjing

--- app/src/main/java/com/example/ourstory/Landing.kt
+++ app/src/main/java/com/example/ourstory/Landing.kt
@@ -4,17 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Window
-import android.view.WindowManager
+import android.os.Looper
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.requestFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_landing)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@Landing, LoginActivity::class.java))
             finish()
         },4000)
