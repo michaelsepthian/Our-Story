ad6895bd1ae6d7f943841bcdc1dc0fc880d2c8e7
Stefanus William Alexander <stefanuswilliam32@gmail.com>
2021-04-12 12:12:14 +0700

    google sign in (done)

--- app/src/main/java/com/example/ourstory/CreateAccActivity.kt
+++ app/src/main/java/com/example/ourstory/CreateAccActivity.kt
@@ -3,20 +3,32 @@ package com.example.ourstory
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_create__acc.*
 import kotlin.collections.HashMap
 
 class CreateAccActivity : AppCompatActivity() {
+
+    companion object{
+        private const val RC_SIGN_IN = 120
+    }
+
     private lateinit var mAuth : FirebaseAuth
     private lateinit var refUsers : DatabaseReference
     private var firebaseUserID: String = ""
+    private lateinit var googleSignInClient : GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +36,14 @@ class CreateAccActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create__acc)
 
         mAuth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         register_button.setOnClickListener {
             registerUser()
@@ -33,7 +53,10 @@ class CreateAccActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
 
+        google_button.setOnClickListener {
+            signIn()
         }
     }
 
@@ -112,5 +135,51 @@ class CreateAccActivity : AppCompatActivity() {
         }
     }
 
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, CreateAccActivity.RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == CreateAccActivity.RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if(task.isSuccessful){
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("CreateAccActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("CreateAccActivity", "Google sign in failed", e)
+                }
+            }else{
+                Log.w("CreateAccActivity", exception.toString())
+            }
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("CreateAccActivity", "signInWithCredential:success")
+                    val intent = Intent(this@CreateAccActivity, DiscoverActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("CreateAccActivity", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
 
 }
--- app/src/main/java/com/example/ourstory/Landing.kt
+++ app/src/main/java/com/example/ourstory/Landing.kt
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
 
 class Landing : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@Landing, LoginActivity::class.java))
             finish()
-        },4000)
+//            if (user != null){
+//                startActivity(Intent(this@Landing, DiscoverActivity::class.java))
+//                finish()
+//            }else{
+//                startActivity(Intent(this@Landing, LoginActivity::class.java))
+//                finish()
+//            }
+        },2000)
 
     }
 }
