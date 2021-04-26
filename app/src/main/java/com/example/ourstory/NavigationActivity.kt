3fa46a8063b63cba878dbb93518bf14b5d64ff83
Nicholas Nathanael <nicholasnathanael098@gmail.com>
2021-04-26 12:05:46 +0700

    update navigasi

--- app/src/main/java/com/example/ourstory/NavigationActivity.kt
+++ app/src/main/java/com/example/ourstory/NavigationActivity.kt
@@ -6,19 +6,35 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_navigation.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    //new
+    //new usiing , NavigationView.OnNavigationItemSelectedListener on top
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
+
+    //new2
+//    lateinit var appBarConfiguration: AppBarConfiguration
+//    internal lateinit var profileCircleImageView: CircleImageView
+//    internal var profileImageUrl =
+//            "https://lh3.googleusercontent.com/-4qy2DfcXBoE/AAAAAAAAAAI/AAAAAAAABi4/rY-jrtntAi4/s640-il/photo.jpg"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
@@ -47,8 +63,33 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
 
+        //new2
+//        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//
+//        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+//        val navView: NavigationView = findViewById(R.id.nav_view)
+//        profileCircleImageView =
+//                navView.getHeaderView(0).findViewById(R.id.imageView)
+//        Glide.with(this)
+//                .load(profileImageUrl)
+//                .into(profileCircleImageView)
+//
+//        appBarConfiguration = AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_myfavorites, R.id.nav_myprofile, R.id.nav_signout
+//        )
+//                .setDrawerLayout(drawerLayout)
+//                .build()
+//
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.nav_home, R.id.nav_myfavorites, R.id.nav_myprofile, R.id.nav_signout), drawerLayout)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
-
+    //new
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_home -> (Toast.makeText(this, "Menu Clicked", Toast.LENGTH_SHORT).show())
@@ -59,7 +100,7 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
-
+    //old
 //    override fun onBackPressed() {
 //        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
 //            drawer_layout.closeDrawer(GravityCompat.START)
@@ -110,6 +151,19 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
 //
 //        drawer_layout.closeDrawer(GravityCompat.START)
 //        return true
+//    }
+
+    //new2
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 //    }
 
 }
